68b58fb42d04a25ac8edaa2749b1cf6375a3eb54
Thomas Thelen <tommythelen@gmail.com>
2019-01-04 15:16:44 -0800

    Change the Member Node to the Coordinating Node
    This variable is used to contact the authentication system on DataONE, and should be talking to the CN rather than the MN. This should also provide more reliable dataset searching.

--- dashboard_local/initial_build.sh
+++ dashboard_local/initial_build.sh
@@ -1,6 +1,6 @@
 sed -e "s|apiHOST|https://girder.local.wholetale.org|g" \
     -e "s|dashboardHOST|https://dashboard.local.wholetale.org|g" \
-    -e "s|dataOneHOST|https://search-stage-2.test.dataone.org|g" \
+    -e "s|dataOneHOST|https://cn-stage-2.test.dataone.org|g" \
     -e "s|authPROVIDER|Globus|g" -i config/environment.js && \
 npm -g install bower && \
 unset NODE_ENV && npm -s install && \
