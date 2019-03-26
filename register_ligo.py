810fefcc9969ebeeab14a8474780757bbdc0ed73
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2019-03-26 18:30:07 -0500

    Fix LIGO registration

--- register_ligo.py
+++ register_ligo.py
@@ -18,19 +18,19 @@ r.raise_for_status()
 headers['Girder-Token'] = r.json()['authToken']['token']
 
 ligo_data = [
-    "https://losc.ligo.org/s/events/GW170104/H-H1_LOSC_4_V1-1167559920-32.hdf5",
-    "https://losc.ligo.org/s/events/GW170104/GW170104_4_template.hdf5",
-    "https://losc.ligo.org/s/events/GW150914/GW150914_4_template.hdf5",
-    "https://losc.ligo.org/s/events/BBH_events_v3.json",
-    "https://losc.ligo.org/s/events/GW151226/GW151226_4_template.hdf5",
-    "https://losc.ligo.org/s/events/GW151226/H-H1_LOSC_4_V2-1135136334-32.hdf5",
-    "https://losc.ligo.org/s/events/LVT151012/L-L1_LOSC_4_V2-1128678884-32.hdf5",
-    "https://losc.ligo.org/s/events/GW170104/L-L1_LOSC_4_V1-1167559920-32.hdf5",
-    "https://losc.ligo.org/s/events/LVT151012/LVT151012_4_template.hdf5",
-    "https://losc.ligo.org/s/events/LVT151012/H-H1_LOSC_4_V2-1128678884-32.hdf5",
-    "https://losc.ligo.org/s/events/GW150914/H-H1_LOSC_4_V2-1126259446-32.hdf5",
-    "https://losc.ligo.org/s/events/GW150914/L-L1_LOSC_4_V2-1126259446-32.hdf5",
-    "https://losc.ligo.org/s/events/GW151226/L-L1_LOSC_4_V2-1135136334-32.hdf5",
+    "https://www.gw-openscience.org/s/events/GW170104/H-H1_LOSC_4_V1-1167559920-32.hdf5",
+    "https://www.gw-openscience.org/s/events/GW170104/GW170104_4_template.hdf5",
+    "https://www.gw-openscience.org/s/events/GW150914/GW150914_4_template.hdf5",
+    "https://www.gw-openscience.org/s/events/BBH_events_v3.json",
+    "https://www.gw-openscience.org/s/events/GW151226/GW151226_4_template.hdf5",
+    "https://www.gw-openscience.org/s/events/GW151226/H-H1_LOSC_4_V2-1135136334-32.hdf5",
+    "https://www.gw-openscience.org/s/events/LVT151012/L-L1_LOSC_4_V2-1128678884-32.hdf5",
+    "https://www.gw-openscience.org/s/events/GW170104/L-L1_LOSC_4_V1-1167559920-32.hdf5",
+    "https://www.gw-openscience.org/s/events/LVT151012/LVT151012_4_template.hdf5",
+    "https://www.gw-openscience.org/s/events/LVT151012/H-H1_LOSC_4_V2-1128678884-32.hdf5",
+    "https://www.gw-openscience.org/s/events/GW150914/H-H1_LOSC_4_V2-1126259446-32.hdf5",
+    "https://www.gw-openscience.org/s/events/GW150914/L-L1_LOSC_4_V2-1126259446-32.hdf5",
+    "https://www.gw-openscience.org/s/events/GW151226/L-L1_LOSC_4_V2-1135136334-32.hdf5",
 ]
 # r = requests.get(api_url + '/repository/lookup', params={'dataId': json.dumps(ligo_data)})
 
@@ -134,7 +134,11 @@ r = requests.post(
     headers=headers,
 )
 
-r = requests.get(api_url + '/dataset', headers=headers)
+r = requests.get(
+    api_url + '/dataset',
+    params={'identifiers': json.dumps(ligo_data)},
+    headers=headers,
+)
 dataSet = [
     {
         'itemId': obj['_id'],
@@ -150,6 +154,7 @@ imageId = r.json()[0]['_id']
 tale = {
     "authors": "Kacper Kowalik",
     "category": "astronomy",
+    "config": {},
     "dataSet": dataSet,
     "description": (
         "### LIGO Detected Gravitational Waves from Black Holes\nOn September 14,"
