11fcc8e40fad9c0f1d800330624c6b1e38dadc42
Mike Lambert <lambert8@illinois.edu>
2021-05-25 18:36:54 +0000

    Add new plugin repos to pull-upstream.sh, print out current branch/changes when pulling

--- pull-upstream.sh
+++ pull-upstream.sh
@@ -18,7 +18,7 @@ fi
 
 # Default parameter to all subdirs
 if [ "$1" == "" ]; then
-	subdirs="dashboard,wholetale,gwvolman,wt_data_manager,wt_home_dir,globus_handler"
+	subdirs="ngx-dashboard,wholetale,gwvolman,girderfs,wt_data_manager,wt_home_dir,globus_handler,wt_versioning,virtual_resources"
 else
 	subdirs="$1"
 fi
@@ -26,5 +26,5 @@ fi
 # Loop over supplied subdirectories and pull from upstream
 for subdir in $(echo "$subdirs" | sed "s/,/ /g")
 do
-	($DEBUG cd $root_dir/src/$subdir && $DEBUG git pull origin master) || exit 1
+	($DEBUG cd $root_dir/src/$subdir && pwd && git status && $DEBUG git pull origin master) || exit 1
 done
