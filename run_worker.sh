54f6980e7985e87049ef1cecbf8bc7dbbf43b461
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2023-02-03 09:52:47 -0600

    Run gwvolman with unprivileged user

--- run_worker.sh
+++ run_worker.sh
@@ -2,15 +2,14 @@
 
 domain=local.wholetale.org
 role=manager,celery
-image=wholetale/gwvolman:latest
+image=wholetale/gwvolman:jail
 registry_user=fido
 registry_pass=secretpass
 r2d_version=wholetale/repo2docker_wholetale:latest
-matlab_file_installation_key=secretkey
+matlab_file_installation_key=$MATLAB_KEY
 node_id=$(docker info --format "{{.Swarm.NodeID}}")
 celery_args="-Q ${role},${node_id} --hostname=${node_id} -c 3"
 
-sudo umount /usr/local/lib > /dev/null 2>&1 || true
 docker stop -t 0 celery_worker >/dev/null 2>&1
 docker rm celery_worker > /dev/null 2>&1
 
@@ -21,9 +20,9 @@ docker rm celery_worker > /dev/null 2>&1
 docker run \
     --name celery_worker \
     --label traefik.enable=false \
-    -e HOSTDIR=/host \
-    -e DEV=true \
+    -e GOSU_USER=wtuser:1000:$(getent group docker | cut -d: -f3) \
     -e DOMAIN=${domain} \
+    -e DEV=true \
     -e REPO2DOCKER_VERSION="${r2d_version}" \
     -e TRAEFIK_NETWORK=wt_traefik-net \
     -e TRAEFIK_ENTRYPOINT=websecure \
@@ -34,17 +33,12 @@ docker run \
     -e WT_VOLUMES_PATH="$PWD"/volumes \
     -e MATLAB_FILE_INSTALLATION_KEY=${matlab_file_installation_key} \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
-    -v /:/host \
-    -v /var/cache/davfs2:/var/cache/davfs2 \
-    -v /run/mount.davfs:/run/mount.davfs \
+    -v /tmp:/tmp \
     -v $PWD/src/gwvolman:/gwvolman \
     -v $PWD/src/girderfs:/girderfs \
+    --mount type=bind,source="$PWD/volumes",target="$PWD/volumes",bind-propagation=rshared \
     --device /dev/fuse \
     --cap-add SYS_ADMIN \
-    --cap-add SYS_PTRACE \
     --security-opt apparmor:unconfined \
     --network wt_celery \
     -d ${image} ${celery_args}
-
-docker exec -ti celery_worker chown davfs2:davfs2 /host/run/mount.davfs
-docker exec -ti celery_worker chown davfs2:davfs2 /host/var/cache/davfs2
